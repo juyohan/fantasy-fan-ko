cd65030431f5326f3c79f27ecdf746f5102b3137
juyohan <dygks8556@naver.com>
2021-10-23 03:36:24 +0900

    fix

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -7,26 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class DemoApplicationTests {
-    @Autowired
-    private lateinit var playerRepository: PlayerRepository
 
     @Test
     fun testDb() {
-//        var play = Player(id = null, playerName = "P.Mils", playerPosition = "G", playerFG = .377f)
-//        var play2 = Player(id = null, playerName = "J.Harden")
-//        var playList : MutableList<Player> = mutableListOf(play, play2)
-//
-//        playList.forEach {
-//            playerRepository.save(it)
-//        }
-//
-//        val findAll = playerRepository.findAll()
-//        println("findAll = ${findAll.javaClass.name}")
-//
-//        findAll.forEach {
-//            println("Player : ${it}")
-//        }
+
     }
 }
