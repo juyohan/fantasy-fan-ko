442359cadf2e85f45552521e718503e929099417
juyohan <dygks8556@naver.com>
2021-10-25 00:20:45 +0900

    id => key 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 }
 
 tasks.withType<KotlinCompile> {
